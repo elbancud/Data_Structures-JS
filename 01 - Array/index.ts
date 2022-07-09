5b057ce89562f57d8427af728bb03413bcc79582
elbancud <elbancud@gmail.com>
2022-07-09 21:38:05 +0800

    chore: array what I know

--- 01 - Array/index.ts
+++ 01 - Array/index.ts
@@ -1,8 +1,30 @@
 /**
  * | =============== What I currently know ===========
  * | String are homogenous container for one data type
- * |
+ * | Interactions
+ * | Push = add element in the position[last]
+ * | Pop = remove element in the last position[last]
  */
 
 const invalidArr = ['a', b, 'c',];
 const validArr = ['a', 'b', 'c', 'd'];
+
+// To Access an array
+const firstElement = validArr[0];
+
+// To add shit in an array
+const addElement = validArr.push('d');
+// To remove last element in an array
+const popElement = validArr.pop();
+// To remove first element in an array
+const shiftElement = validArr.shift()
+// To display every elements in the array
+/**
+ * | =========== One can use the following =========
+ * | For each
+ * | map
+ * | for (convention)
+ */
+validArr.forEach((element) => element);
+validArr.map(element => element);
+for (let i = 0; i <= validArr.length; i++) validArr[i];
