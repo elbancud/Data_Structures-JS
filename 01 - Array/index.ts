b6bdbe87e867c570544869a46cefe5a1919f34f2
elbancud <elbancud@gmail.com>
2022-07-09 21:31:49 +0800

    chore: arrays

---
+++ 01 - Array/index.ts
@@ -0,0 +1,8 @@
+/**
+ * | =============== What I currently know ===========
+ * | String are homogenous container for one data type
+ * |
+ */
+
+const invalidArr = ['a', b, 'c',];
+const validArr = ['a', 'b', 'c', 'd'];
